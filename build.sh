f202eb41922b9c48b5a38990c35b0e603b5dd593
Enric Cervera <ecervera@uji.es>
2018-06-08 14:29:53 +0200

    Remove ROS package

--- build.sh
+++ build.sh
@@ -38,12 +38,3 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j 1
 cd ..
-
-echo "Configuring and building SSM_LinearArray ROS package ..."
-
-cd ROS/SSM_LinearArray/
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j 1
-cd ../../../
